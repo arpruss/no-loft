b23ad6324b203dc12e015702bd9ea8a444162d80
Alexander Pruss <arpruss@gmail.com>
2026-07-11 14:01:09 -0500

    foo

---
+++ app/src/main/java/mobi/omegacentauri/mutehome/mutehome.java
@@ -0,0 +1,26 @@
+package mobi.omegacentauri.mutehome;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+public class mutehome extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+
+}
